f22893cebe33d82c1f29caadb6da7cba5dfbed66
BlackCat <155682600+3lackC4t@users.noreply.github.com>
2024-06-09 03:42:58 +0000

    Build basic logic for performing a -yank-

--- main.py
+++ main.py
@@ -5,11 +5,30 @@ import yaml
 
 
 class Anchors:
+  def __init__(self) -> None:
+    self.anchor_path = os.path.join('~/.config/Anchors', 'Anchors.yml')
+    
+    with open(self.anchor_path, 'r') as anchors:
+      self.anchors = yaml.safe_load(anchors)
+    
+  def anchor_yank(self, alias):
+    if alias in self.anchors:
+      os.system(f'cd {self.anchors[alias]}')
+
+  def anchor_add(self, alias, path):
+    with open(self.acnhor_path, 'a') as anchors:
+      new_anchor = {'alias': alias, 'path': path}
+      anchors.write(yaml.safe_dump(new_anchor))
+
+  def anchor_remove(self):
+    pass
+
+class AnchorConfigurator(Anchors):
   def __init__(self):
+    super().__init__()
     self.default_config = [{'alias': 'home', 'path': '~/'},
                           {'alias':'config', 'path':'~/.config'}]
-    self.anchor_path = os.path.join('~/.config/Anchors', 'Anchors.yml')
-
+    
   def creat_config(self):
     if os.path.exists(self.anchor_path):
       print("anchor.yml already exists. Checking for configurations...")
@@ -38,15 +57,6 @@ class Anchors:
   def print_anchor_list(self):
     with open(self.anchor_path) as anchors:
       pprint.pp(anchors, indent=2, width=60)
-  
-  def anchor_yank(self):
-    pass
-
-  def anchor_add(self):
-    pass  
-
-  def anchor_remove(self):
-    pass
 
 
 def build_parser():
@@ -57,12 +67,26 @@ def build_parser():
   parser.add_argument('-y', '--yank', type=str, default='home', 
                       help="Move to the anchors directory"
   parser.add_arguement('-l', '--list', type=str, 
-                       action='store_true',help='list current anchors and their paths'
+                       action='store_true',help='list current anchors and their paths')
 
-  return parser
+  args = parser.parse_args()
+  return args
 
 def main():
-  pass
+  args = build_parser()
+  anchor = Anchors()
+  anchor_config = AnchorConfigurator()
+
+  # Check if config exists
+  anchor_config.check_config()
+
+  if args.add:
+    anchor.anchor_add(args.add)
+  elif args.remove:
+    pass
+  elif args.yank:
+    anchor.anchor_yank(args.yank)
+  
 
 if __name__ == "__main__":
   pass
