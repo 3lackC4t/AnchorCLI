b4c704a90ca46723400746d03b9e83e275d691ce
3lackC4t <C.S.Minty@proton.me>
2024-06-10 04:21:24 +0000

    Creates the anchors.yml file now as well as its directory

--- src/application.py
+++ src/application.py
@@ -5,14 +5,36 @@ import yaml
 
 class Anchor:
     def __init__(self):
-        self.default_anchors = [{'alias': 'home', 'path': '~/'},
-                                {'alias': 'config', 'path': '~/.config'}]
+        self.default_anchors = [{'alias': 'home', 'path': f'/home/{os.getlogin()}'},
+                                {'alias': 'config', 'path': f'/home/{os.getlogin()}/.config'}]
+        
+        if not os.path.exists(f'/home/{os.getlogin()}/.config/AnchorCLI/anchors.yml'):
+            os.makedirs(f'/home/{os.getlogin()}/.config/AnchorCLI', exist_ok=True)
+            with open(f'/home/{os.getlogin()}/.config/AnchorCLI/anchors.yml', 'w') as anchors:
+                anchors.write(yaml.safe_dump(self.default_anchors))
 
+            self.anchor_path = os.path.join(f'/home/{os.getlogin()}/.config/AnchorCLI', 'anchors.yml')
+        else:
+            self.anchor_path = os.path.join(f'/home/{os.getlogin()}/.config/AnchorCLI', 'anchors.yml')
+        
+        with open(self.anchor_path, 'r') as anchors:
+            self.current_anchors = yaml.safe_load(anchors)
+
+    def anchor_move(self, alias):
+        for anchor in self.current_anchors:
+            if anchor['alias'] == alias:
+                print(anchor['path'])
+                os.chdir(anchor['path'])
+
+    def anchor_add(self, alias, path):
+        pass
 
 def initialize():
     pass
 
 def main():
-    print(os.path.exists('~/.config/AnchorCLI/anchors.yml'))
-    
+    anchor = Anchor()
+    print(os.getcwd())
+    anchor.anchor_move('config')
+    print(os.getcwd())
 
