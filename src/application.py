73d89ee3b4b8428d6916e94ff3c43c0fb5938673
3lackC4t <C.S.Minty@proton.me>
2024-06-11 21:27:00 +0000

    list, reset, add, and do functions complete

--- src/application.py
+++ src/application.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-import pprint
 import yaml
 
 class Anchor:
@@ -29,9 +28,10 @@ class Anchor:
         except PermissionError as PermErr:
             print('Permission denied, command must be run as root')
 
-    def anchor_add(self, path: str, alias: str):
-        with open(self.anchor_path, 'a') as anchors:
-            anchors.write(yaml.safe_dump({'alias': f'{alias}', 'path': f'{path}'}))
+    def add_anchor(self, alias: str, path: str):
+        self.current_anchors.append({'alias': f'{alias}', 'path': f'{path}'})
+        with open(self.anchor_path, 'w') as anchors:
+            anchors.write(yaml.safe_dump(self.current_anchors))
 
     def list_anchors(self):
         for index, anchor in enumerate(self.current_anchors):
@@ -44,7 +44,7 @@ class Anchor:
         with open(self.anchor_path, 'r') as anchors:
             self.current_anchors = yaml.safe_load(anchors)
 
-    def set_default_anchors(self, anchor_args):
+    def set_default_anchors(self, anchor_args): 
         new_default = []
         for arg in anchor_args:
             new_default.append(arg)
@@ -53,46 +53,53 @@ class Anchor:
 
 
 def build_parser():
-    parser = argparse.ArgumentParser(
-        prog='AnchorCLI',
-        description='''
-        A tool for easily executing shell commands in directories using easy to remember and user
-        configurable aliases.
-        '''
-        )
+    parser = argparse.ArgumentParser(description='''Manage anchors and execute shell 
+                                     commands in specified directories.''')
 
-    general = parser.add_argument_group('group')
-    yml_config = parser.add_mutually_exclusive_group()
-
-    yml_config.add_argument('-n', '--new',
+    optional = parser.add_mutually_exclusive_group()
+    optional.add_argument('-a', 
+                          '--add', 
+                          metavar=('alias', 'path'), 
+                          nargs=2, 
+                          help='Add a new anchor')
+    optional.add_argument('-r', 
+                          '--reset', 
+                          action='store_true', 
+                          help='Reset anchors to program defaults')
+    optional.add_argument('-l', 
+                          '--list', 
+                          action='store_true', 
+                          help='List all anchors')
+    parser.add_argument('alias', 
+                        nargs='?', 
+                        type=str, 
+                        help='Alias and shell command to execute')
+    parser.add_argument('shell_command',
+                        nargs=argparse.REMAINDER,
                         type=str,
-                        action='store',
-                        nargs=2,
-                        default=f"{os.getcwd()}",
-                        help='''Takes two arguments, an alias and a filepath, 
-                                and stores it in anchors.yml''')
-    yml_config.add_argument('-r', '--remove',
-                            type=str,
-                            action='store',
-                            help='remove an anchor from anchors.yml')
-    general.add_argument('do',
-                        type=str,
-                        action='store',
-                        nargs=2,
-                        help='''takes two arguments, an alias and a command, 
-                                and executes the command in the chosen directory''')
-    yml_config.add_argument('-l', '--list',
-                         action='store_true',
-                         help='lists currently saved anchors')
+                        help="The shell command to be executed")
 
     args = parser.parse_args()
+
     return args
 
 
 def main():
     args = build_parser()
     anchor = Anchor()
-    print(args)
-    anchor.do_anchor(args.do[0], args.do[1])
-   
 
+    if args.add:
+        alias, path = args.add
+        anchor.add_anchor(alias, path)
+    elif args.reset:
+        anchor.reset_anchors()
+        print("anchors.yml reset to program defaults. See documentation for details")
+    elif args.list:
+        print("Listing all available anchors...")
+        anchor.list_anchors()
+    elif args.alias:
+        alias = args.alias
+        command = " ".join(args.shell_command)
+        anchor.do_anchor(alias, command)
+    else:
+        parser.print_help()
